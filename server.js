8bb3f707ff5c5b2b64f27d24b6b3db30cf82e6b2
Anton <antonbobotov@gmail.com>
2026-04-18 04:10:36 +0300

    Use dynamic PORT for hosting

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -11,5 +11,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log('http://localhost:3000');
+  console.log(`http://localhost:${PORT}`);
 });
